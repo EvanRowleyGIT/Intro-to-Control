b0bf82c5d13375b94c86a5c0e38b0e49bd730a13
EvanRowleyGIT <143126749+EvanRowleyGIT@users.noreply.github.com>
2023-09-21 13:21:53 -0600

    Add files via upload

---
+++ Control Structures.py
@@ -0,0 +1,55 @@
+# car_color = "Blue"
+# car_make = "Tesla"
+
+# if car_color == "Blue" or car_color == "blue":
+#     if car_make == "Tesla":
+#          print("My car is a Blue Tesla")
+#     else:
+#          print("My car is blue")
+# elif car_color == "Red" or car_color == "red":
+#      print("My car is a ferrari")
+# else:
+#      print("I just have a bike")
+
+# for loop
+
+
+
+# sports = ['soccer', 'basketball', 'hockey, football']
+# print(sports)
+
+# for item in range(10):
+#     print(item)
+
+# While Loop
+# i = 1
+# while i < 6:
+#     print("I'm still going")
+#     i += 1
+
+# phrase = input("What phrase would you like to encrypt? ")
+# shift = int(input)("How many letters would you like to shift? ")
+
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# newAlpha = ''
+
+# def getShifted(shift):
+#     for i in range(len(alphabet)):
+#         offset = (i + shift) % len(alphabet)
+#         newAlpha = alphabet[offset]
+#         return newAlpha
+#     print("Here is your message: {result}")
+# def encode(input):
+#     result = ''
+#     for i in range (len(input)):
+#         index = alphabet.index
+
+# getShifted(shift)
+
+# encode(input)
+
+# for i in range(50):
+#     if i > 24:
+#         break
+#     else:
+#         print(i)
